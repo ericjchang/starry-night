87630163907fe98618ff9350d94fde701da976a6
ericjchang <eric.jaspchang@gmail.com>
2023-06-20 08:51:13 +0700

    -install d3-celestial -add "map" component & config -add "editor" component -remove react test script & unused script

---
+++ src/Components/Editor/Editor.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Editor() {
+  return (
+    <>
+      <div>Editor</div>
+      <p>Location</p>
+      <p>Dedication</p>
+      <p>Date</p>
+      <p>Design</p>
+      <button>Generate PDF</button>
+    </>
+  );
+}
+
+export default Editor;
---
+++ src/Components/Map.js
@@ -0,0 +1,105 @@
+import React, { useState, useEffect } from 'react';
+import celestial from 'd3-celestial';
+
+const Celestial = celestial.Celestial();
+window.Celestial = Celestial;
+
+function Map() {
+  const [date, setDate] = useState(new Date());
+  const [location, setLocation] = useState({
+    lat: 0,
+    long: 0,
+  });
+
+  const config = {
+    container: 'map',
+    width: 700,
+    form: true,
+    advanced: false,
+    controls: false,
+    interactive: true,
+    disableAnimations: false,
+
+    zoomlevel: 0.9,
+    zoomextend: 1,
+
+    projection: 'airy',
+
+    lines: {
+      graticule: { show: false },
+      equatorial: { show: false },
+      ecliptic: { show: false },
+      galactic: { show: false },
+      supergalactic: { show: false },
+    },
+    datapath: 'https://ofrohn.github.io/data/',
+    planets: {
+      show: false,
+    },
+
+    dsos: {
+      show: false,
+      names: false,
+    },
+
+    constellations: {
+      names: false,
+      lines: true, // Show constellation lines, style below
+      lineStyle: { stroke: '#cccccc', width: 1, opacity: 0.6 },
+    },
+
+    mw: {
+      show: false,
+    },
+
+    background: {
+      fill: '#000',
+      stroke: '#ffffff',
+      opacity: 1,
+      width: 2,
+    },
+
+    stars: {
+      colors: false,
+      size: 6,
+      limit: 8,
+      exponent: -0.28,
+      designation: false,
+      propername: false,
+    },
+  };
+
+  useEffect(() => {
+    console.log('run');
+    Celestial.display(config);
+  }, []);
+
+  useEffect(() => {
+    Celestial.skyview({ date: date, location: { lat: location.lat, long: location.long } });
+    console.log(location, date);
+  }, [date, location]);
+
+  const changeLocation = () => {
+    const d = new Date('14 April 2022 00:00:00 GMT');
+    setDate(d);
+    setLocation({
+      lat: 52.520008,
+      long: 13.404954,
+    });
+  };
+
+  return (
+    <div>
+      <p>Date : {date.toString()}</p>
+      <p>Lat : {location.lat}</p>
+      <p>Long : {location.long}</p>
+      <div id='map-container'>
+        <div id='map'></div>
+      </div>
+      <div id='celestial-form'></div>
+      <button onClick={changeLocation}>Change</button>
+    </div>
+  );
+}
+
+export default Map;
