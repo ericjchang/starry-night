37a73984a96ac37e67d2c4fc24004dcee17e9c40
ericjchang <eric.jaspchang@gmail.com>
2023-08-15 18:19:08 +0700

    -update map styling -update result page to showing dedication and editor text

--- src/App.js
+++ src/App.js
@@ -2,29 +2,44 @@ import React from 'react';
 import Page from './components/Page';
 import Map from './components/Map';
 import Editor from './components/Editor';
-import { Provider } from 'react-redux';
 
-import store from './store';
+import { connect } from 'react-redux';
+import { toDMS } from './helpers/CoordinateFormater';
+import { dateToString } from './helpers/DateFormater';
 
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
+function App({ day, month, year, long, lat, city, dedication }) {
   return (
     <div className='container d-flex'>
-      <Provider store={store}>
-        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
-        <Page>
+      <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+      <Page>
+        <div className='main-content'>
           <Map />
-          <p className=''>dedication</p>
-          <p className=''>city</p>
-          <p className=''>coordinates</p>
-          <p className=''>date</p>
-        </Page>
-        <Editor />
-      </Provider>
+          <div className='dedication-container'>
+            <p className='title'>{dedication}</p>
+            <p className='detail'>{city}</p>
+            <p className='detail'>{`${toDMS(lat, 'lat')} ${toDMS(long, 'lon')}`}</p>
+            <p className='detail'>{dateToString(day, month, year)}</p>
+          </div>
+        </div>
+      </Page>
+      <Editor />
     </div>
   );
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    day: state.DateReducer.day,
+    month: state.DateReducer.month,
+    year: state.DateReducer.year,
+    long: state.LocationReducer.long,
+    lat: state.LocationReducer.lat,
+    city: state.LocationReducer.name,
+    dedication: state.DedicationReducer.text,
+  };
+};
+
+export default connect(mapStateToProps)(App);
--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import './styles.css';
 import { useSelector } from 'react-redux';
 import celestial from 'd3-celestial';
 
